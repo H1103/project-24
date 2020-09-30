a4c40d8c1172aafd7a77aff47697e6274a32e495
H1103 <71260380+H1103@users.noreply.github.com>
2020-09-30 22:50:07 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,17 +1,22 @@
-class Ground{
-    constructor(x, y, width, height){
-        var options ={
-            'isStatic':true
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        World.add(world,this.body);
-    }
+class Ground {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true,
+        'restitution':0,
+        'friction':0,
+        'density':1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+        
+      World.add(world, this.body);
+      }
     display(){
-        var pos=this.body.position;
-        rectMode(CENTER);
-        fill('purple');
-        rect(pos.x, pos.y, this.width, this.height);
-    }
-};
+      rectMode(CENTER);
+  
+      fill("white");
+  
+      rect(this.body.position.x, this.body.position.y, this.width, this. height);
+      }
+    };
