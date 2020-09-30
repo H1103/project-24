902b69eb51dbbbf2c791ed021f3da78f60c267d2
H1103 <71260380+H1103@users.noreply.github.com>
2020-09-30 22:48:59 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,18 +1,21 @@
-class Ball{
-    constructor(x,y){
-        var options ={
-            'restitution':0.3,
-            'friction':0.5,
-            'density':1.2
-        }
-        this.body = Bodies.circle(x, y, 20, options);
-
-        World.add(world, this.body);
-    }
+class Ball {
+    constructor(x, y, radius) {
+      var options = {
+        isStatic: false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius
+        
+      World.add(world, this.body);
+      }
     display(){
-        var pos = this.body.position;
-        ellipseMode(RADIUS);
-        fill("yellow");
-        ellipse(pos.x, pos.y, 20, 20);
+      ellipseMode(RADIUS);
+  
+      fill("yellow");
+  
+      circle(this.body.position.x, this.body.position.y, this.radius);
     }
-};
+    };
